caf17d29833720bc97c6e864a3effcf756be62d4
Nils Hesse <nphesse@gmail.com>
2014-10-10 22:50:11 +0200

    This seems to work reliable now.
    Seems to work even with tricky setups. You may have to change
    sensibility.
    We'll see if it's realy usable or if it has some bad things left for me.

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,12 +1,12 @@
 #include <stdint.h>
 
-#define INT16_MAX 0x7fff
-
 #define DEBUG                     1
 
 #define TRIGGER_PIN               12
 #define LED_PIN                   13
 #define TRIGGER_LEVEL             LOW
+
+#define MAX_TRIGGERS_PER_SECOND   4
 
 // Defines if readings should be averaged.
 #define NUM_AVERAGE_READINGS      0
@@ -29,56 +29,45 @@
 
 // As soon as the difference of the current reading to the previous/average
 // reading is at least as high as this number, trigger the endstop
-#define TRIGGER_HIGH_MARK         64
+#define TRIGGER_HIGH_MARK         128
 // As soon as the negative difference of the current reading to the
 // previous/average reading is at least as high as this number, reset
 // the endstop
 #define TRIGGER_LOW_MARK          16
 
+#define MIN_TIME_PER_TRIGGER      (1000 / MAX_TRIGGERS_PER_SECOND)
+
 // The analog pins on which the FSRs can be read
 uint8_t analogIn[3] = {A0, A1, A2};
 
-uint8_t  triggered[4] = {0, 0, 0, 0};
+uint8_t  triggered[3] = {0, 0, 0};
 unsigned long lastBaseLevelReset = 0;
 uint16_t baseLevel[3] = {0, 0, 0};
 uint16_t reading[3] = {0, 0, 0};
+uint16_t highMark[3] = {0, 0, 0};
+uint16_t lowMark[3] = {0, 0, 0};
 
-uint8_t flapDetectionBuf[FLAP_DETECTION_BUFFER];
-uint8_t  flapPos = 0;
-uint8_t  flapDataPoints = 0;
+unsigned long lastTriggerTime[3] = {0, 0, 0};
+unsigned long lastDebugOutput = 0;
+uint8_t  baseLevelReadPending[3] = {0, 0, 0};
 
-uint8_t  detectFlapping(uint8_t changed) {
-    flapDetectionBuf[flapPos] = changed;
-    if(flapPos >= FLAP_DETECTION_BUFFER) {
-        flapPos = 0;
-    } else {
-        flapPos++;
+void readBaseLevelForFSR(uint8_t in) {
+    uint32_t fsr = 0;
+    for(uint8_t r = 0; r < BASE_LEVEL_AVG_READINGS; r++) {
+        fsr += analogRead(analogIn[in]);
     }
+    fsr /= BASE_LEVEL_AVG_READINGS;
+    baseLevel[in] = fsr;
 
-    if(flapDataPoints < FLAP_DETECTION_BUFFER) {
-        flapDataPoints++;
-    }
+    triggered[in] = 0;
 
-    uint8_t changes = 0;
-    for(uint8_t i = 0; i < flapDataPoints; i++) {
-        changes += flapDataPoints;
-    }
-
-    return (changes > (flapDataPoints/2));
+    highMark[in] = min(1023, int16_t(baseLevel[in] + TRIGGER_HIGH_MARK));
 }
 
 void readBaseLevel() {
     for(uint8_t i = 0; i < 3; i++) {
-        uint32_t fsr = 0;
-        for(uint8_t r = 0; r < BASE_LEVEL_AVG_READINGS; r++) {
-            fsr += analogRead(analogIn[i]);
-        }
-        fsr /= BASE_LEVEL_AVG_READINGS;
-        baseLevel[i] = fsr;
-
-        triggered[i] = 0;
+        readBaseLevelForFSR(i);
     }
-    triggered[3] = 0;
 }
 
 void setup()
@@ -120,37 +109,25 @@ void loop()
         reading[i] = analogRead(analogIn[i]);
 #endif
 
-        int16_t lowMark = max(0, int16_t(baseLevel[i] - TRIGGER_LOW_MARK));
-        int16_t highMark = min(INT16_MAX, int16_t(baseLevel[i] + TRIGGER_HIGH_MARK));
-
-        if(reading[i] > highMark) {
-            if(!triggered[i]) {
+        if(reading[i] > highMark[i]) {
+            if(!triggered[i] && (millis() - lastTriggerTime[i]) >= MIN_TIME_PER_TRIGGER) {
+                lastTriggerTime[i] = millis();
                 lastBaseLevelReset = millis();
                 triggerChanged = 1;
                 triggered[i] = 1;
             }
-        } else if(triggered[i] && (reading[i] < lowMark || baseLevel[i] < TRIGGER_LOW_MARK)) {
-            triggerChanged = 1;
-            triggered[i] = 0;
+
+            if(lowMark[i] <= TRIGGER_LOW_MARK || reading[i] > (lowMark[i] - TRIGGER_LOW_MARK)) {
+                // If we're reading higher values than before update the trigger low mark
+                lowMark[i] = max(0, int16_t(reading[i] - TRIGGER_LOW_MARK));
+            }
+        } else if((reading[i] < lowMark[i] || baseLevel[i] < TRIGGER_LOW_MARK)) {
+            if(triggered[i]) {
+                lastTriggerTime[i] = millis();
+                triggerChanged = 1;
+                triggered[i] = 0;
+            }
         }
-
-    }
-
-    int16_t accuRead = reading[0] + reading[1] + reading[2];
-    int16_t accuBase = baseLevel[0] + baseLevel[1] + baseLevel[2];
-
-    int16_t accuLowMark = max(0, int16_t(accuBase - TRIGGER_LOW_MARK*2));
-    int16_t accuHighMark = min(INT16_MAX, int16_t(accuBase + TRIGGER_HIGH_MARK*2));
-
-    if(accuRead > accuHighMark) {
-        if(!triggered[3]) {
-            lastBaseLevelReset = millis();
-            triggerChanged = 1;
-            triggered[3] = 1;
-        }
-    } else if(triggered[3] && (accuRead < accuLowMark || accuBase < TRIGGER_LOW_MARK)) {
-        triggerChanged = 1;
-        triggered[3] = 0;
     }
 
     if((millis() - lastBaseLevelReset) > (unsigned long)(RESET_AFTER_SECONDS * 1000)) {
@@ -161,14 +138,7 @@ void loop()
         triggerChanged = 1;
     }
 
-    if(detectFlapping(triggerChanged)) {
-        // We are flapping, for safety assume triggered.
-        triggered[3] = 1;
-
-        Serial.println("FLAPPING");
-    }
-
-    if(triggered[0] + triggered[1] + triggered[2] + triggered[3] > 0) {
+    if(triggered[0] + triggered[1] + triggered[2] > 0) {
         digitalWrite(TRIGGER_PIN, TRIGGER_LEVEL);
         digitalWrite(LED_PIN, HIGH);
     } else {
@@ -177,33 +147,38 @@ void loop()
     }
 
 #if DEBUG > 0
-    if(triggerChanged) {
+    if(millis() - lastDebugOutput > 500 || triggerChanged) {
+        lastDebugOutput = millis();
+
+        if(triggerChanged) {
+            Serial.print("Low mark: a:");
+            Serial.print(lowMark[0]);
+            Serial.print(" b:");
+            Serial.print(lowMark[1]);
+            Serial.print(" c:");
+            Serial.println(lowMark[2]);
+        }
+
         Serial.print("Triggered: a:");
         Serial.print(triggered[0]);
         Serial.print(" b:");
         Serial.print(triggered[1]);
         Serial.print(" c:");
-        Serial.print(triggered[2]);
-        Serial.print(" d:");
-        Serial.println(triggered[3]);
+        Serial.println(triggered[2]);
 
         Serial.print("Reading: a:");
         Serial.print(reading[0]);
         Serial.print(" b:");
         Serial.print(reading[1]);
         Serial.print(" c:");
-        Serial.print(reading[2]);
-        Serial.print(" d:");
-        Serial.println(accuRead);
+        Serial.println(reading[2]);
 
         Serial.print("BaseLevel: a:");
         Serial.print(baseLevel[0]);
         Serial.print(" b:");
         Serial.print(baseLevel[1]);
         Serial.print(" c:");
-        Serial.print(baseLevel[2]);
-        Serial.print(" d:");
-        Serial.println(accuBase);
+        Serial.println(baseLevel[2]);
 
         Serial.print("Last reset:");
         Serial.print(millis() - lastBaseLevelReset);
